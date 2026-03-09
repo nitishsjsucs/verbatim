158187e7ac2f6180408952e5c45ec26a1a7f0430
Arya Adhikari <98677324+AryaAdhikari09@users.noreply.github.com>
2026-03-09 15:24:15 +0530

    refactor(scripts): update full system reset corrected and 5 other files

--- web/src/lib/permissions.ts
+++ web/src/lib/permissions.ts
@@ -49,10 +49,3 @@ export const teamMember = ac.newRole({
     dashboard: ["view"],
     evidence: ["view", "submit"],
 })
-
-export const chief = ac.newRole({
-    actionable: ["view"],
-    dashboard: ["view"],
-    evidence: ["view"],
-    delay: ["view", "justify"],
-})
